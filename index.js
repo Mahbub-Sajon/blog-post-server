9edf5781fbc33d8a52ef73760cf4d4eff22a1664
Mahbub-Sajon <sajon.mahbub@gmail.com>
2022-09-05 16:15:45 +0600

    database connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors  = require ('cors');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,10 +9,30 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const uri = "mongodb+srv://blog-post:2SsoO5tbXLlGn594@cluster0.79xvt8g.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+await client.connect.db("blogs")
+    }
+    finally {
+        // await client.close();
+     }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Hello World!')
-  })
-  
-  app.listen(port, () => {
+})
+
+app.listen(port, () => {
     console.log(`Blog port running  ${port}`)
-  })
+});
+
+  //2SsoO5tbXLlGn594
+  //blog-post
